dda2bff3430e5527994dcecbe60c6fa2b20b36a5
igornfaustino <igor@nfaustino.com>
2022-01-04 14:07:22 -0300

    change Encrypter to be more generic

--- src/utils/helpers/encrypter.spec.js
+++ src/utils/helpers/encrypter.spec.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcrypt')
 class Encrypter {
-  async compare (password, hashedPassword) {
-    return bcrypt.compare(password, hashedPassword)
+  async compare (value, hashedValue) {
+    return bcrypt.compare(value, hashedValue)
   }
 }
 
@@ -9,7 +9,7 @@ describe('Encrypter', () => {
   it('should return true if bcrypt returns true', async () => {
     const sut = new Encrypter()
 
-    const isSame = await sut.compare('any_password', 'hashed_password')
+    const isSame = await sut.compare('value', 'hashed_value')
 
     expect(isSame).toBe(true)
   })
@@ -18,7 +18,7 @@ describe('Encrypter', () => {
     const sut = new Encrypter()
     bcrypt.output = false
 
-    const isSame = await sut.compare('any_password', 'hashed_password')
+    const isSame = await sut.compare('value', 'hashed_value')
 
     expect(isSame).toBe(false)
   })
@@ -26,9 +26,9 @@ describe('Encrypter', () => {
   it('should return calls bcrypt with right params', async () => {
     const sut = new Encrypter()
 
-    await sut.compare('any_password', 'hashed_password')
+    await sut.compare('value', 'hashed_value')
 
-    expect(bcrypt.value).toBe('any_password')
-    expect(bcrypt.hashedValue).toBe('hashed_password')
+    expect(bcrypt.value).toBe('value')
+    expect(bcrypt.hashedValue).toBe('hashed_value')
   })
 })
