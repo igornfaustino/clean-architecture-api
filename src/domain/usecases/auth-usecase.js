b62f71ae0bde2a150ab58f1e84371430005a4fc4
igornfaustino <igor@nfaustino.com>
2022-01-04 10:19:38 -0300

    simplify code

--- src/domain/usecases/auth-usecase.js
+++ src/domain/usecases/auth-usecase.js
@@ -11,8 +11,7 @@ module.exports = class AuthUseCase {
     if (!email) throw new MissingParamError('email')
     if (!password) throw new MissingParamError('password')
     const user = await this.loadUserByEmailRepository.load(email)
-    if (!user) return null
-    const isValid = await this.encrypter.compare(password, user.password)
+    const isValid = user && await this.encrypter.compare(password, user.password)
     if (!isValid) return null
     return this.tokenGenerator.generate(user.id)
   }
