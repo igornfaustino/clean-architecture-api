6940541e281e629d1a9db630f876bc4fa3976153
igornfaustino <igor@nfaustino.com>
2022-01-06 16:16:44 -0300

    remove unused variables

--- src/infra/helpers/mongo-helper.js
+++ src/infra/helpers/mongo-helper.js
@@ -1,12 +1,12 @@
 const { MongoClient } = require('mongodb')
 
 module.exports = {
-  async connect (uri, dbName) {
+  async connect (uri) {
     this.client = await MongoClient.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
-    this.db = await this.client.db(dbName)
+    this.db = await this.client.db()
   },
 
   async disconnect () {
